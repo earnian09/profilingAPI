ec4e29c9b3627120494a88eaf7814f26575c7b22
earnian09 <81209243+earnian09@users.noreply.github.com>
2024-02-29 22:20:07 +0800

    Update server.js

--- server.js
+++ server.js
@@ -231,15 +231,15 @@ app.get('/getAllEmployees/:department', (req, res) => {
 
 // Login
 app.post('/login', (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    //res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    //res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
+    //res.setHeader('Access-Control-Allow-Credentials', true);
     const emp_ID = req.body.emp_ID;
     const password = req.body.password;
 
